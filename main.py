5fda4e7af8a6220b92b1f409ddd02dda513baee8
JaimeAlvarezRodriguez <arj2031181008@upatlacomulco.edu.mx>
2023-12-17 12:55:55 -0600

    v0.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
-Proyect: RADAR SAR
-Author: Jaime Alvarez Rodriguez
-
+Proyect:        RADAR SAR
+Author:         Jaime Alvarez Rodriguez
+Description:    User interface to control and processing signals from RADAR SAR to computer 
 """
 import tkinter
 from AppRadar.new_window import New_window
