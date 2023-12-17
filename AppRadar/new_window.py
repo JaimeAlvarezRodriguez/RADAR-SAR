69d9cdff96c759702b53cca52c31a9a4f1d5efe8
JaimeAlvarezRodriguez <arj2031181008@upatlacomulco.edu.mx>
2023-12-17 12:13:58 -0600

    v2

---
+++ AppRadar/new_window.py
@@ -0,0 +1,14 @@
+import tkinter
+
+
+class New_window(tkinter.Toplevel):
+    def __init__(self, master, tittle, geometry):
+        super().__init__(master=master)
+        self.title(tittle)
+        self.geometry(geometry)
+    def create_widgets():
+        pass
+    def place_widgets():
+        pass
+
+    
